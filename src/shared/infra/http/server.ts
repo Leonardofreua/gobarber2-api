f8b1e7aabf16aa4dbbda40700b965a6870b59191
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-06-16 16:45:07 -0300

    feat: created app server

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3333, () => {
+  // eslint-disable-next-line
+  console.log('Server started on port 3333!');
+});
